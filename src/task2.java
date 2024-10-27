add9f5e58c18219b43ee16852de8d1c1cbed991f
M1tn1ck <mamonov@duck.com>
2024-10-28 00:22:24 +0300

    new

---
+++ src/task2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+        String b = "";
+        int c = 1;
+
+        for (int i = 1; i < a.length(); i++){
+            if (a.charAt(i) == a.charAt(i - 1))
+                c++;
+
+            else{
+                b += a.charAt(i - 1)  +""+ c;
+                c = 1;
+            }
+        }
+
+        if (a.length() > 0)
+            b += a.charAt(a.length() - 1)+""  + c;
+
+        System.out.print(b);
+
+
+
+
+
+
+
+
+
+    }
+}
